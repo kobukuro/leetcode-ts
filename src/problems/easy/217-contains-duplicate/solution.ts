30ae53058a2dce1ddf8b83d713cbf1121b82de72
Peter Chen <sanpeter992000@gmail.com>
2025-10-24 10:47:04 +0800

    Enhance containsDuplicate documentation with complexity analysis.

--- src/problems/easy/217-contains-duplicate/solution.ts
+++ src/problems/easy/217-contains-duplicate/solution.ts
@@ -1,3 +1,12 @@
+// Tags: Hash Set
+
+/**
+ * Time complexity: O(n).
+ * - We do search() and insert() for n times and each operation takes constant time.
+ *
+ * Space complexity: O(n).
+ * - The space used by a Set is linear with the number of elements in it.
+ */
 export function containsDuplicate(nums: number[]): boolean {
     const seen = new Set<number>();
     for (const num of nums) {
