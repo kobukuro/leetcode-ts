ef17734a1e351d03c683fa11a66048097a54b742
Peter Chen <sanpeter992000@gmail.com>
2025-10-24 10:35:02 +0800

    Add non-null assertion to Map.get() in twoSum.

--- src/problems/easy/1-two-sum/solution.ts
+++ src/problems/easy/1-two-sum/solution.ts
@@ -14,7 +14,7 @@ export function twoSum(nums: number[], target: number): number[] {
     for (let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
         if (lookup.has(complement)) {
-            return [lookup.get(complement), i];
+            return [lookup.get(complement)!, i];
         }
         lookup.set(nums[i], i);
     }
