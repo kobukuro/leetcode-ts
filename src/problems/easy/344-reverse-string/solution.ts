96e4f3e5fc3dfb71da078c80725a92ee25aba05f
Peter Chen <sanpeter992000@gmail.com>
2026-01-20 11:12:58 +0800

    Optimize Reverse String to skip redundant self-swap when pointers meet.

--- src/problems/easy/344-reverse-string/solution.ts
+++ src/problems/easy/344-reverse-string/solution.ts
@@ -13,7 +13,7 @@
 export function reverseString(s: string[]): void {
     let l = 0;
     let r = s.length - 1;
-    while (l <= r) {
+    while (l < r) {
         [s[l], s[r]] = [s[r], s[l]];
         l++;
         r--;
